01e6eb05fc4f965cb234b884a220f41340163bb5
UncleJerry <jared.myhrberg@gmail.com>
2019-08-09 10:49:00 -0700

    added condition to sort by time if now reviews

--- tests/utils/sortsByReviewTime.test.js
+++ tests/utils/sortsByReviewTime.test.js
@@ -3,22 +3,22 @@ const { sortByReview } = require('../../lib/utils/sortByReview');
 it('sorts by review', () => {
   
   const posts = [
-    { title: 'sixth', good: 0, bad: 0 },
-    { title: 'fifth', good: 0, bad: 0 },
-    { title: 'fifth', good: 0, bad: 4 },
-    { title: 'third', good: 3, bad: 0 },
-    { title: 'first', good: 5, bad: 2 },
-    { title: 'fourth', good: 10, bad: 30 },
-    { title: 'second', good: 1, bad: 1  }
+    { title: 'sixth', time: '2019-08-09T17:46:34.583Z', good: 0, bad: 0 },
+    { title: 'fifth', time: '2019-02-09T17:46:34.583Z', good: 0, bad: 0 },
+    { title: 'fifth', time: '2019-08-09T17:46:34.583Z', good: 0, bad: 4 },
+    { title: 'third', time: '2019-08-09T17:46:34.583Z', good: 3, bad: 0 },
+    { title: 'first', time: '2019-08-09T17:46:34.583Z', good: 5, bad: 2 },
+    { title: 'fourth', time: '2019-08-09T17:46:34.583Z', good: 10, bad: 30 },
+    { title: 'second', time: '2019-08-09T17:46:34.583Z', good: 1, bad: 1  }
   ];
   const expected = [
-    { title: 'third', good: 3, bad: 0 },
-    { title: 'first', good: 5, bad: 2 },
-    { title: 'second', good: 1, bad: 1 },
-    { title: 'sixth', good: 0, bad: 0 },
-    { title: 'fifth', good: 0, bad: 0 },
-    { title: 'fourth', good: 10, bad: 30 },
-    { title: 'fifth', good: 0, bad: 4 }
+    { title: 'third', time: '2019-08-09T17:46:34.583Z', good: 3, bad: 0 },
+    { title: 'first', time: '2019-08-09T17:46:34.583Z', good: 5, bad: 2 },
+    { title: 'second', time: '2019-08-09T17:46:34.583Z', good: 1, bad: 1 },
+    { title: 'fifth', time: '2019-02-09T17:46:34.583Z', good: 0, bad: 0 },
+    { title: 'sixth', time: '2019-08-09T17:46:34.583Z', good: 0, bad: 0 },
+    { title: 'fourth', time: '2019-08-09T17:46:34.583Z', good: 10, bad: 30 },
+    { title: 'fifth', time: '2019-08-09T17:46:34.583Z', good: 0, bad: 4 }
   ];
 
   const result = sortByReview(posts);
